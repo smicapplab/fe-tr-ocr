a3ce06b22dbc4293d065e82893b8bba6adf5034a
Steve Torrefranca <s.torrefranca@gmail.com>
2024-11-27 17:08:30 +0800

    chatbot

--- src/routes/ocr/(components)/nav.svelte
+++ src/routes/ocr/(components)/nav.svelte
@@ -232,7 +232,7 @@
 			{/if}
 		{/each}
 
-		{#if !isCollapsed}
+		<!-- {#if !isCollapsed}
 			{#if $ocrCountStore}
 				<div class="py-10 text-center text-xs">
 					<div class="bg-orange-50 p-2 text-center text-xs">
@@ -240,6 +240,6 @@
 					</div>
 				</div>
 			{/if}
-		{/if}
+		{/if} -->
 	</nav>
 </div>
