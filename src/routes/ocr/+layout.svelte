1fc405a313512ca01a0681c168ecee0a80853c27
Steve Torrefranca <s.torrefranca@gmail.com>
2024-12-02 18:14:27 +0800

    Update +layout.svelte

--- src/routes/ocr/+layout.svelte
+++ src/routes/ocr/+layout.svelte
@@ -1,5 +1,8 @@
 <div class="bg-red-50 text-sm p-2">
-	<span class="font-bold">**** Latest Update****</span>. Chatbot demo has been archived. To visit <a href="https://fe-chatbot-olive.vercel.app/login" class="italic underline">click this link</a>.  For other archived versions or apps, contact s.torrefranca@gmail.com  
+	Demonstration Scope: An OCR text and data extraction demo for ‘Credit Approval Memorandum’ files.
+	This simple demo utilizes a basic AI model with limited context processing, serving as a proof of
+	concept for automated document analysis. Note: This is an initial demonstration, requiring
+	additional files and data to improve accuracy and enhance AI capabilities.
 </div>
 
 <div class="p-5 lg:p-10">
